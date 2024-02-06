707e129eeb4d811b223963ebf2d3476f0f79d85d
AbinSurendran <abin7.dev@gmail.com>
2024-02-07 00:50:37 +0530

    add redux persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -7,7 +7,7 @@ import {
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
-import OAuth from '../components/OAuth';
+// import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -95,7 +95,7 @@ export default function SignIn() {
                 "Sign In"
               )}
             </Button>
-            <OAuth />
+            {/* <OAuth /> */}
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont have an account?</span>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { useReducer } from './user/userSlice'
+// import { configureStore } from '@reduxjs/toolkit'
+// import { useReducer } from './user/userSlice'
+
+// export const store = configureStore({
+//   reducer: {
+//     user: useReducer,
+//   },
+// })
+
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  theme: themeReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: useReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);

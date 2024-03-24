cc545ae13cac47312a3afd6cf9edf5090349be4b
AbinSurendran <abin7.dev@gmail.com>
2024-03-24 20:57:41 +0530

    add update user api route

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -303,3 +303,24 @@ export default function DashProfile() {
     </div>
   );
 }
+
+
+// import {useSelector} from 'react-redux';
+
+
+
+
+// export default function DashProfile() {
+//   const {currentUser} = useSelector(state => state.user)
+//   return (
+//     <div>
+//     <h1>Profile</h1>
+//     <form>
+//       <div className='w-32 h-32'>
+//         <img src={currentUser.profilePicture} alt="user" className='rounded-full' />
+//       </div>
+      
+//     </form>
+//     </div>
+//   )
+// }

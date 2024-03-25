ec8876f7b1097b90234753a3a8b927e0f26a5a96
AbinSurendran <abin7.dev@gmail.com>
2024-03-25 21:38:22 +0530

    add admin functionality to the user

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
       default:
         "https://th.bing.com/th/id/OIP.2ZXXKN7zakCiYeNrq5-b_gHaHa?w=216&h=216&c=7&r=0&o=5&dpr=1.3&pid=1.7",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
